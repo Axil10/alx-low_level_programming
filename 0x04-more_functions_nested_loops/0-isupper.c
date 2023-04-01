8a8844b770417624bd27e2a60d29e91ebdb5397e
Axil10 <forziti1@gmail.com>
2023-04-01 09:28:13 -0700

    my first c

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -1,17 +1,21 @@
-#include "main.h"
-
-/**
- * _isupper - uppercase letters
- * @c: character to check
- *
- * Return: 0 or 1
+/*
+ * File: 0-isupper.c
+ * Auth: Brennan D Baraban
  */
 
+#include "holberton.h"
 
+/**
+ * _isupper - Checks for uppercase characters.
+ * @c: The character to be checked.
+ *
+ * Return: 1 if character is uppercase, 0 otherwise.
+ */
 int _isupper(int c)
 {
 	if (c >= 'A' && c <= 'Z')
 		return (1);
+
 	else
 		return (0);
 }
